6c2c31d862dc3c8ca27b745abb6be2d4109f5ed9
bob_mervell <b0b0bm3rv3ll@gmail.com>
2025-03-21 19:30:44 +0100

    feat: Updated themes, added more parameters in the theme style

--- lib/Themes/app_themes.dart
+++ lib/Themes/app_themes.dart
@@ -1,77 +1,188 @@
 import 'package:flutter/material.dart';
 
-final ThemeData lightTheme = ThemeData(
+// Couleurs principales
+const Color darkColor = Color(0xFF4F4739);
+const Color lightColor = Color(0xFFF1EBE4);
+const Color accentColor = Colors.blue; // Couleur d'accentuation
+
+ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
-  primarySwatch: Colors.grey,
-  primaryColor: Colors.black,
-  scaffoldBackgroundColor: const Color(0xFFF1EBE4),
-  canvasColor: const Color(0xFFF1EBE4),
-  cardColor: const Color(0xFFF1EBE4),
-  dividerColor: const Color(0xFF4F4739),
-
-  textTheme: TextTheme(
-    displayLarge: TextStyle(color: const Color(0xFF4F4739), fontFamily: 'Monospace'),
-    displayMedium: TextStyle(color: const Color(0xFF4F4739), fontFamily: 'Monospace'),
-    displaySmall: TextStyle(color: const Color(0xFF4F4739), fontFamily: 'Monospace'),
-    headlineLarge: TextStyle(color: const Color(0xFF4F4739), fontFamily: 'Monospace'),
-    headlineMedium: TextStyle(color: const Color(0xFF4F4739), fontFamily: 'Monospace'),
-    headlineSmall: TextStyle(color: const Color(0xFF4F4739), fontFamily: 'Monospace'),
-    titleLarge: TextStyle(color: const Color(0xFF4F4739), fontFamily: 'Monospace'),
-    titleMedium: TextStyle(color: const Color(0xFF4F4739), fontFamily: 'Monospace'),
-    titleSmall: TextStyle(color: const Color(0xFF4F4739), fontFamily: 'Monospace'),
-    bodyLarge: TextStyle(color: const Color(0xFF4F4739), fontFamily: 'Monospace'),
-    bodyMedium: TextStyle(color: const Color(0xFF4F4739), fontFamily: 'Monospace'),
-    bodySmall: TextStyle(color: const Color(0xFF4F4739), fontFamily: 'Monospace'),
+  primaryColor: darkColor,
+  colorScheme: ColorScheme.light(
+    primary: darkColor,
+    secondary: accentColor,
+    surface: lightColor,
+    onSurface: darkColor,
   ),
 
-  iconTheme: IconThemeData(color: Colors.black),
-  primaryIconTheme: IconThemeData(color: Colors.black),
+
+
+ textTheme: TextTheme(
+    displayLarge: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold, color: darkColor, fontFamily: 'Monospace'),
+    displayMedium: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold,color: darkColor, fontFamily: 'Monospace'),
+    displaySmall: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold,color: darkColor, fontFamily: 'Monospace'),
+    headlineLarge: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold,color: darkColor, fontFamily: 'Monospace'),
+    headlineMedium: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold,color: darkColor, fontFamily: 'Monospace'),
+    headlineSmall: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold,color: darkColor, fontFamily: 'Monospace'),
+    titleLarge: TextStyle(fontSize: 28.0,color: darkColor, fontFamily: 'Monospace'),
+    titleMedium: TextStyle(fontSize: 24.0,color: darkColor, fontFamily: 'Monospace'),
+    titleSmall: TextStyle(fontSize: 20.0,color: darkColor, fontFamily: 'Monospace'),
+    bodyLarge: TextStyle(fontSize: 18.0,color: darkColor, fontFamily: 'Monospace'),
+    bodyMedium: TextStyle(fontSize: 16.0,color: darkColor, fontFamily: 'Monospace'),
+    bodySmall: TextStyle(fontSize: 14.0,color: darkColor, fontFamily: 'Monospace'),
+  ),
+
+  // AppBar Theme
   appBarTheme: AppBarTheme(
-    backgroundColor: Colors.white,
-    iconTheme: IconThemeData(color: Colors.black),
-    titleTextStyle: TextStyle(color: Colors.black, fontFamily: 'Monospace'),
+    color: lightColor,
+    iconTheme: IconThemeData(color: darkColor),
+    titleTextStyle: TextStyle(color: darkColor, fontSize: 20.0, fontWeight: FontWeight.bold),
   ),
+
+  // Input Decoration Theme
+  inputDecorationTheme: InputDecorationTheme(
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8.0),
+      borderSide: BorderSide(color: darkColor, width: 2.0),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8.0),
+      borderSide: BorderSide(color: accentColor, width: 2.0),
+    ),
+    hintStyle: TextStyle(color: darkColor.withAlpha(150)),
+    labelStyle: TextStyle(color: darkColor),
+  ),
+
+  // Card Theme
+  cardTheme: CardTheme(
+    color: lightColor,
+    shadowColor: darkColor.withAlpha(50),
+    elevation: 4.0,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+    margin: EdgeInsets.all(8.0),
+  ),
+
+  // Bottom Navigation Bar Theme
   bottomNavigationBarTheme: BottomNavigationBarThemeData(
-    backgroundColor: Colors.white,
-    selectedItemColor: Colors.black,
-    unselectedItemColor: Colors.grey,
+    backgroundColor: darkColor,
+    selectedItemColor: lightColor,
+    unselectedItemColor: lightColor.withAlpha(150),
   ),
-);
+
+  // Dialog Theme
+  dialogTheme: DialogTheme(
+    backgroundColor: lightColor,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+  ),
+
+  // Divider Theme
+  dividerTheme: DividerThemeData(color: darkColor.withAlpha(75), thickness: 1.0),
+
+  // Chip Theme
+  chipTheme: ChipThemeData(
+    backgroundColor: lightColor,
+    selectedColor: darkColor,
+    labelStyle: TextStyle(color: darkColor),
+    secondaryLabelStyle: TextStyle(color: lightColor),
+  ),
+
+  /* // ElevatedButton Theme
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: darkColor, // Couleur du texte
+      foregroundColor: lightColor, // Couleur du texte sur surface
+      iconColor: accentColor,
+
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      textStyle: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+      elevation: 4.0, // Ombre portée
+    ),
+  ), */
+
+/*   // TextButton Theme
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(
+      backgroundColor: darkColor, // Couleur du texte
+      foregroundColor: lightColor, // Couleur du texte sur surface
+      iconColor: accentColor,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+      textStyle: TextStyle(fontSize: 16.0, fontWeight: FontWeight.normal,color: accentColor),
+    ),
+  ), */
+
+/*   // OutlinedButton Theme
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      backgroundColor: darkColor, // Couleur du texte
+      foregroundColor: lightColor, // Couleur du texte sur surface
+      iconColor: accentColor,
+
+      side: BorderSide(color: darkColor, width: 2.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      textStyle: TextStyle(fontSize: 16.0, fontWeight: FontWeight.normal, color: darkColor),
+    ),
+  ), */
+
+/*   // IconButton Theme
+  iconTheme: IconThemeData(
+    color: accentColor, // Couleur des icônes
+  ), */
+
+/*     // Floating Action Button Theme
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: darkColor,
+    foregroundColor: lightColor,
+  ), */
+
+  /* // Slider Theme
+  sliderTheme: SliderThemeData(
+    activeTrackColor: darkColor,
+    inactiveTrackColor: darkColor.withAlpha(75),
+    thumbColor: accentColor,
+    valueIndicatorColor: accentColor,
+  ), */
+
+/*   // Switch Theme
+  switchTheme: SwitchThemeData(
+    thumbColor: WidgetStateProperty.resolveWith<Color>((states) {
+      if (states.contains(WidgetState.selected)) {
+        return accentColor;
+      }
+      return darkColor.withAlpha(150);
+    }),
+    trackColor: WidgetStateProperty.resolveWith<Color>((states) {
+      if (states.contains(WidgetState.selected)) {
+        return accentColor.withAlpha(150);
+      }
+      return darkColor.withAlpha(75);
+    }),
+  ), */
+
+/*   // Checkbox Theme
+  checkboxTheme: CheckboxThemeData(
+    checkColor: WidgetStateProperty.resolveWith<Color>((states) {
+      return lightColor;
+    }),
+    fillColor: WidgetStateProperty.resolveWith<Color>((states) {
+      if (states.contains(WidgetState.selected)) {
+        return accentColor;
+      }
+      return darkColor.withAlpha(75);
+    }),
+  ), */
+
+/*   // Radio Theme
+  radioTheme: RadioThemeData(
+    fillColor: WidgetStateProperty.resolveWith<Color>((states) {
+      if (states.contains(WidgetState.selected)) {
+        return accentColor;
+      }
+      return darkColor.withAlpha(75);
+    }),
+  ), */
 
 
-// Thème sombre avec effet encre numérique
-ThemeData darkTheme = ThemeData(
-  brightness: Brightness.dark,
-  primarySwatch: Colors.grey,
-  primaryColor: Colors.white,
-  scaffoldBackgroundColor: Colors.black,
-  canvasColor: Colors.black,
-  cardColor: Colors.grey[800],
-  dividerColor: Colors.white,
-  textTheme: TextTheme(
-    displayLarge: TextStyle(color: Colors.white, fontFamily: 'Monospace'),
-    displayMedium: TextStyle(color: Colors.white, fontFamily: 'Monospace'),
-    displaySmall: TextStyle(color: Colors.white, fontFamily: 'Monospace'),
-    headlineLarge: TextStyle(color: Colors.white, fontFamily: 'Monospace'),
-    headlineMedium: TextStyle(color: Colors.white, fontFamily: 'Monospace'),
-    headlineSmall: TextStyle(color: Colors.white, fontFamily: 'Monospace'),
-    titleLarge: TextStyle(color: Colors.white, fontFamily: 'Monospace'),
-    titleMedium: TextStyle(color: Colors.white, fontFamily: 'Monospace'),
-    titleSmall: TextStyle(color: Colors.white, fontFamily: 'Monospace'),
-    bodyLarge: TextStyle(color: Colors.white, fontFamily: 'Monospace'),
-    bodyMedium: TextStyle(color: Colors.white, fontFamily: 'Monospace'),
-    bodySmall: TextStyle(color: Colors.white, fontFamily: 'Monospace'),
-  ),
-  iconTheme: IconThemeData(color: Colors.white),
-  primaryIconTheme: IconThemeData(color: Colors.white),
-  appBarTheme: AppBarTheme(
-    backgroundColor: Colors.black,
-    iconTheme: IconThemeData(color: Colors.white),
-    titleTextStyle: TextStyle(color: Colors.white, fontFamily: 'Monospace'),
-  ),
-  bottomNavigationBarTheme: BottomNavigationBarThemeData(
-    backgroundColor: Colors.black,
-    selectedItemColor: Colors.white,
-    unselectedItemColor: Colors.grey,
-  ),
+
 );
