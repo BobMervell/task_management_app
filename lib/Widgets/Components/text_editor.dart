9c93a404dce1d4ef54ce2c4b1cfd4b05b63878c8
bob_mervell <b0b0bm3rv3ll@gmail.com>
2025-03-25 12:52:25 +0100

    docs: add commentary for text editor

--- lib/Widgets/Components/text_editor.dart
+++ lib/Widgets/Components/text_editor.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 
+/// A rich text editor widget that allows for formatted text input.
+///
+/// This widget includes a toolbar for text formatting and an editor area for text input.
 class RichTextEditor extends StatefulWidget {
   final String editorTitle;
   final QuillController controller;
@@ -9,38 +12,44 @@ class RichTextEditor extends StatefulWidget {
     super.key,
     required this.editorTitle,
     required this.controller,
-    });
-
+  });
 
   @override
   RichTextEditorState createState() => RichTextEditorState();
 }
 
+/// State class for the RichTextEditor widget.
+///
+/// Manages the visibility of the toolbar and animations for showing/hiding the toolbar.
 class RichTextEditorState extends State<RichTextEditor> with SingleTickerProviderStateMixin {
   final double heightRatio;
   bool _isToolbarVisible = false;
   late AnimationController _heightAnimationController;
   late Animation<double> _heightAnimation;
+
   RichTextEditorState({
     this.heightRatio = 3,
   });
 
-  
-@override
+  @override
   void initState() {
     super.initState();
+    // Initialize the animation controller for the toolbar
     _heightAnimationController = AnimationController(
       duration: const Duration(milliseconds: 300),
       vsync: this,
     );
+    // Define the animation for the toolbar height
     _heightAnimation = Tween<double>(begin: 0.0, end: 40.0).animate(
       CurvedAnimation(parent: _heightAnimationController, curve: Curves.easeInOut),
     );
+    // Show the toolbar if it is initially visible
     if (_isToolbarVisible) {
       _heightAnimationController.forward();
     }
   }
 
+  /// Toggles the visibility of the toolbar and animates the height change.
   void _toggleToolbar() {
     setState(() {
       _isToolbarVisible = !_isToolbarVisible;
@@ -52,22 +61,22 @@ class RichTextEditorState extends State<RichTextEditor> with SingleTickerProvide
     });
   }
 
-
+  /// Configures the toolbar for the Quill editor.
   QuillSimpleToolbarConfig toolbarConfig(BuildContext context) {
     return QuillSimpleToolbarConfig(
-    showFontFamily: false,
-    showLineHeightButton: true,
-    showClearFormat: false,
-    showAlignmentButtons: true,
-    showSearchButton: false,
-    showInlineCode: false,
-    showQuote: false,
-    showListBullets: false,
-    showUndo: false,
-    showRedo: false,
-    sectionDividerColor: Theme.of(context).dividerColor,
-  );
-}
+      showFontFamily: false,
+      showLineHeightButton: true,
+      showClearFormat: false,
+      showAlignmentButtons: true,
+      showSearchButton: false,
+      showInlineCode: false,
+      showQuote: false,
+      showListBullets: false,
+      showUndo: false,
+      showRedo: false,
+      sectionDividerColor: Theme.of(context).dividerColor,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -75,10 +84,15 @@ class RichTextEditorState extends State<RichTextEditor> with SingleTickerProvide
       children: [
         Row(
           children: [
-            Text(widget.editorTitle,style:Theme.of(context).textTheme.headlineSmall,),
+            Text(
+              widget.editorTitle,
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
             IconButton(
               icon: Icon(_isToolbarVisible ? Icons.expand_less : Icons.expand_more),
-              onPressed: () { _toggleToolbar();},
+              onPressed: () {
+                _toggleToolbar();
+              },
             ),
           ],
         ),
@@ -88,50 +102,51 @@ class RichTextEditorState extends State<RichTextEditor> with SingleTickerProvide
     );
   }
 
+  /// Creates an animated container for the toolbar.
   AnimatedBuilder animatedToolbarContainer(BuildContext context) {
     return AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (context, child) {
-          return Container(
-            alignment: Alignment.bottomLeft,
-            height: _heightAnimation.value,
-            child: child,
-          );
-        },
-        child: QuillSimpleToolbar(
-          controller: widget.controller,
-          config: toolbarConfig(context),
-        ),
-      );
+      animation: _heightAnimation,
+      builder: (context, child) {
+        return Container(
+          alignment: Alignment.bottomLeft,
+          height: _heightAnimation.value,
+          child: child,
+        );
+      },
+      child: QuillSimpleToolbar(
+        controller: widget.controller,
+        config: toolbarConfig(context),
+      ),
+    );
   }
 
+  /// Creates a container for the text editor.
   Container textEditorContainer(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).canvasColor,
-          borderRadius: BorderRadius.circular(8.0),
-          border: Border.all(
-            color:Theme.of(context).dividerColor ,
-            width: 2.0
-          ),
+      decoration: BoxDecoration(
+        color: Theme.of(context).canvasColor,
+        borderRadius: BorderRadius.circular(8.0),
+        border: Border.all(
+          color: Theme.of(context).dividerColor,
+          width: 2.0,
         ),
-        child: Row(
-          children: [
-            Flexible(
-              child: QuillEditor.basic(
-                controller: widget.controller,
-                scrollController: ScrollController(),
-                focusNode: FocusNode(),
-                config : QuillEditorConfig(
-                  minHeight: MediaQuery.of(context).size.height/heightRatio ,
-                  maxHeight: 3 * MediaQuery.of(context).size.height/heightRatio ,
-                  padding: EdgeInsets.all(8)
-                ),
+      ),
+      child: Row(
+        children: [
+          Flexible(
+            child: QuillEditor.basic(
+              controller: widget.controller,
+              scrollController: ScrollController(),
+              focusNode: FocusNode(),
+              config: QuillEditorConfig(
+                minHeight: MediaQuery.of(context).size.height / heightRatio,
+                maxHeight: 3 * MediaQuery.of(context).size.height / heightRatio,
+                padding: EdgeInsets.all(8),
               ),
             ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
   }
 }
-
