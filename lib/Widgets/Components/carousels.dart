0b48149e32360fa2002f56c7e180faf7b13a05a5
bob_mervell <b0b0bm3rv3ll@gmail.com>
2025-03-25 13:32:39 +0100

    docs: add commentary for carousel

--- lib/Widgets/Components/carousels.dart
+++ lib/Widgets/Components/carousels.dart
@@ -3,33 +3,40 @@ import 'package:flutter/material.dart' hide CarouselController;
 import 'package:task_management_app/Widgets/task_summary_card.dart';
 import 'package:task_management_app/Providers/task_provider.dart';
 
-
+/// A carousel widget that displays a list of project summary cards.
+///
+/// This widget uses the CarouselSlider package to display a horizontally scrollable list of project summary cards.
 class ProjectCarousel extends StatefulWidget {
   final TaskProvider taskProvider;
   final double height;
   final double viewportFraction;
+
   const ProjectCarousel({
     super.key,
     required this.taskProvider,
     this.height = 300,
     this.viewportFraction = 0.4,
   });
+
   @override
   ProjectCarouselState createState() => ProjectCarouselState();
 }
 
+/// State class for the ProjectCarousel widget.
+///
+/// Manages the configuration options for the carousel.
 class ProjectCarouselState extends State<ProjectCarousel> {
   late CarouselOptions carouselOptions;
-  
-  
+
   @override
   void initState() {
     super.initState();
+    // Initialize the carousel options with the provided height and viewport fraction
     carouselOptions = CarouselOptions(
       autoPlay: false,
       height: widget.height,
       autoPlayCurve: Curves.easeInBack,
-      viewportFraction:widget.viewportFraction,
+      viewportFraction: widget.viewportFraction,
       enableInfiniteScroll: false,
       pageSnapping: false,
       scrollDirection: Axis.horizontal,
